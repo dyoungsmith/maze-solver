c6a3e2590776d58fe919158ec9622d0254c4c7d6
Danielle YoungSmith <dyoungsmith@gmail.com>
2017-04-07 16:30:14 -0700

    loads mazes

--- solver-tests.js
+++ solver-tests.js
@@ -5,3 +5,5 @@
 // const maze = '(3,3)-[34,14,12,6,77,5,1,19,9]';
 // const maze = `+---+---+---+\n| E         |\n+---+   +   +\n|     * |   |\n+   +   +   +\n|   | S     |\n+---+---+---+`;
 
+// Test maze solution
+// const solution = ['up', 'up', 'left'];
--- solver.js
+++ solver.js
@@ -1,4 +1,14 @@
 // BIG THING TO FIGURE OUT: how to represent the maze
 
+const fs = require('fs');
+
+
 // Loading mazes from mazes.txt
-// 
+let mazeArr = [];   // Contains the full maze description as original string
+fs.readFile('./mazes.txt', 'utf8', (err, mazes) => {
+    if (err) console.error(err);
+    else {
+        mazeArr = mazes.split('\n');
+        // console.log(mazeArr)
+    }
+})
