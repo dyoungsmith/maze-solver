c9aeadc692692bd1bdc93ca588e0ad8124bf99a7
Danielle YoungSmith <dyoungsmith@gmail.com>
2017-04-17 13:59:51 -0400

    cleaned up

--- solver.js
+++ solver.js
@@ -1,16 +1,12 @@
-// BIG THING TO FIGURE OUT: how to represent the maze -- AS A GRAPH!! :D
-
 const fs = require('fs');
 const MAZES = require('./mazes.txt');
 const TEST_MAZE = require('./maze-test.txt');
 
-
-// Constructor for each cell (represented as nodes)
-class CellNode {
+// Cell constructor (cells as nodes)
+class Cell {
     constructor(idx, code) {
         // features
         this.idx = idx;
-        this.code = code;
         this.start = null;
         this.end = null;
         this.mine = null;
@@ -21,14 +17,16 @@ class CellNode {
         this.right = null;
         this.up = null;
         this.down = null;
+
+        this.setState(code);
     }
 
     // Determine and set the state of each cell
-    setState() {
+    setState(code) {
         const states = ['up', 'right', 'down', 'left', 'start', 'end', 'mine'];
 
         states.forEach((state, i) => {
-            if (this.code & Math.pow(2, i)) {
+            if (code & Math.pow(2, i)) {
                 if (i < 4) this.edges.push(state);
                 else this[state] = true;
             };
@@ -36,37 +34,47 @@ class CellNode {
     }
 
     // Create references to edge nodes
-    // This will be used recursively in the Maze constructor
+    // This will be used recursively in the Maze constructor ???
     linkEdges() {
 
     }
 }
 
-// // Build Maze: FIGURE OUT STRUCTURE
+// // Build Maze: FIGURE OUT BEST STRUCTURE
 class Maze {
     constructor(mazeStr) {
-        this.mazeStr = mazeStr;
         this.height = 0;
         this.width = 0;
         this.cellArr = [];
+
+        this.start = {};
+        this.end = {};
+
+        this.setData(mazeStr);
     }
 
-    setData() {
+    setData(mazeStr) {
         // remove leading/trailing brackets; separate dimensions from structure
         // edited = ['h,w', 'cellCodes']
-        let edited = this.mazeStr.substring(1, maze.length - 1).split(')-[');
+        let edited = mazeStr.substring(1, maze.length - 1).split(')-[');
 
         // set Maze dimensions
         const dims = edited[0].split(',');
         this.height = Number(dims[0]);
         this.width = Number(dims[1]);
 
-        // set Maze cells
+        // set cell code array
         const cellStrs = edited[1].split(',');
         cellStrs.forEach(str => {
             this.cellArr.push(Number(str));
         });
     }
+
+    // // Build the maze, transforming each cell
+    // // Replace each cell code with node, then link based on idx???
+    // // OR find start and end cells thru bitwise, then perform A* on edges from start to end
+    // if (currCell.start) this.start = currNode;
+    // if (currCell.end) this.end = currNode;
 }
 
 // Maze Solver: A* Pathfinding Algorithm (https://en.wikipedia.org/wiki/A*_search_algorithm)
@@ -91,13 +99,9 @@ fs.readFile(MAZES, 'utf8', (err, mazes) => {
     else {
         let mazeArr = mazes.trim().split('\n'); // separate mazes
 
-        // edit maze data to be usable
+        // CALL MAZE CONSTRUCTOR + SOLVER ON EACH MAZE
         mazeArr.forEach(maze => {
-            // CALL MAZE CONSTRUCTOR + SOLVER ON EACH MAZE
 
         });
     }
 });
-// .then(mazeArr => {
-
-// })
