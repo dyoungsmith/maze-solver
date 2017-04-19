96b962b52a7e60649906a37023505471cd812a48
Danielle YoungSmith <dyoungsmith@gmail.com>
2017-04-19 16:43:11 -0400

    binary heap class implemented

--- solver-tests.js
+++ solver-tests.js
@@ -1,9 +1,13 @@
 // it reads maze files
 // run solver on maze-test.txt
 
-// Test maze creation
-// const maze = '(3,3)-[34,14,12,6,77,5,1,19,9]';
-// const maze = `+---+---+---+\n| E         |\n+---+   +   +\n|     * |   |\n+   +   +   +\n|   | S     |\n+---+---+---+`;
+// Test cell state update
+
+// Test Maze start and end nodes
+
+// Test maze graph creation
+// const mazeInfo = '(3,3)-[34,14,12,6,77,5,1,19,9]';
+
 
 // Test maze solution
 // const solution = ['up', 'up', 'left'];
--- solver.js
+++ solver.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const MAZES = require('./mazes.txt');
 const TEST_MAZE = require('./maze-test.txt');
 
-// Cell constructor (cells as nodes)
+// Cell constructor (cells are nodes)
 class Cell {
-    constructor(idx, code) {
+    constructor(code, idx) {
         // features
         this.idx = idx;
         this.start = null;
@@ -33,30 +33,146 @@ class Cell {
         });
     }
 
-    // Create references to edge nodes
-    // This will be used recursively in the Maze constructor ???
-    linkEdges() {
+    // // Create references to edge nodes
+    // // This will be used recursively in the Maze constructor ???
+    // linkEdges() {
 
+    // }
+}
+
+// BinaryHeap constructor for priority queue
+class BinaryHeap {
+    constructor(scoreFn) {
+        this.content = [];
+        this.scoreFn = scoreFn;
+    }
+
+    push(elem) {
+        // add elem to content array
+        this.content.push(elem);
+        // allow it to sink down to create the heap
+        this.sinkDown(this.content.length - 1);
+    }
+
+    pop() {
+        const result = this.content[0];
+        const end = this.content.pop();
+        // If there are elems left, put the end elem at the start, then bubble up
+        if (this.content.length) {
+            this.content[0] = end;
+            this.bubbleUp(0);
+        }
+        return result;
+    }
+
+    remove(node) {
+        const i = this.content.indexOf(node);
+        // Fill the hole for the removed node
+        const end = this.content.pop();
+
+        if (i !== this.content.length - 1) {
+            this.content[i] = end;
+            if (this.scoreFn(end) < this.scoreFn(node)) {
+                this.sinkDown(i);
+            }
+            else {
+                this.bubbleUp(i);
+            }
+        }
+    }
+
+    size() {
+        return this.content.length;
+    }
+
+    rescoreElement(node) {
+        this.sinkDown(this.content.indexOf(node));
+    }
+
+    sinkDown(n) {
+        // Grab the elem for sinking
+        const elem = this.content[n];
+        // idx = 0 is the farthest the elem can sink
+        while (n > 0) {
+            // Compute parent idx and grab the parent
+            const parentN = ((n + 1) >> 1) - 1;
+            const parent = this.content[parentN];
+            // Swap elems if necessary
+            if (this.scoreFn(elem) < this.scoreFn(parent)) {
+                [elem, parent] = [parent, elem];    // CHECK IF THIS WORKS
+                // Update n to continue
+                n = parentN;
+            }
+            else break;
+        }
+    }
+
+    bubbleUp(n) {
+        // Get target elem and its score
+        const length = this.content.length;
+        const elem = this.content[n];
+        const elemScore = this.scoreFn(elem);
+
+        while (true) {
+            // Compute the indices of the child elements.
+            var child2N = (n + 1) << 1;
+            var child1N = child2N - 1;
+            // This is used to store the new position of the element, if any.
+            var swap = null;
+            var child1Score;
+            // If the first child exists (is inside the array)...
+            if (child1N < length) {
+                // Look it up and compute its score.
+                var child1 = this.content[child1N];
+                child1Score = this.scoreFunction(child1);
+
+                // If the score is less than our element's, we need to swap.
+                if (child1Score < elemScore) {
+                swap = child1N;
+                }
+            }
+
+            // Do the same checks for the other child.
+            if (child2N < length) {
+                var child2 = this.content[child2N];
+                var child2Score = this.scoreFunction(child2);
+                if (child2Score < (swap === null ? elemScore : child1Score)) {
+                swap = child2N;
+                }
+            }
+
+            // If the element needs to be moved, swap it, and continue.
+            if (swap !== null) {
+                this.content[n] = this.content[swap];
+                this.content[swap] = elem;
+                n = swap;
+            }
+            // Otherwise, we are done.
+            else {
+                break;
+            }
+        }
     }
 }
 
-// // Build Maze: FIGURE OUT BEST STRUCTURE
+// Build Maze as a graph: cells are nodes, openings are edges
 class Maze {
     constructor(mazeStr) {
         this.height = 0;
         this.width = 0;
         this.cellArr = [];
-
         this.start = {};
         this.end = {};
 
         this.setData(mazeStr);
+        this.getTargetCells();
     }
 
+    // Translate and set input strings to usable data
     setData(mazeStr) {
         // remove leading/trailing brackets; separate dimensions from structure
         // edited = ['h,w', 'cellCodes']
-        let edited = mazeStr.substring(1, maze.length - 1).split(')-[');
+        let edited = mazeStr.substring(1, mazeStr.length - 1).split(')-[');
 
         // set Maze dimensions
         const dims = edited[0].split(',');
@@ -73,9 +189,41 @@ class Maze {
     // // Build the maze, transforming each cell
     // // Replace each cell code with node, then link based on idx???
     // // OR find start and end cells thru bitwise, then perform A* on edges from start to end
-    // if (currCell.start) this.start = currNode;
-    // if (currCell.end) this.end = currNode;
-}
+
+    // Find start and end cells; O(n = cellArr.length)
+    getTargetCells() {
+        this.cellArr.forEach((cellCode, i) => {
+            if (cellCode & 16) {
+                this.start = new Cell(cellCode, i);
+            }
+            if (cellCode & 32) {
+                this.end = new Cell(cellCode, i);
+            }
+        })
+    }
+
+    // ~~~~~~~~~~~~~~ A* Implementation ~~~~~~~~~~~~~~~~~~~~
+    // WRITE ABOUT HOW I CHOSE THIS ONE
+    
+    // Returns the full path
+    reconstructPath(node) {
+        let curr = node;
+        let path = [];
+        while (curr.parent) {
+            // Will probably have to edit to get directions, not just parent node
+            path.unshift(curr);
+            curr = curr.parent;
+        }
+        return path;
+    }
+
+    // Constructs a BinaryHeap scoring by f(n) values
+    getHeap() {
+        return new BinaryHeap(node => {
+            return node.f;
+        });
+    }
+ }
 
 // Maze Solver: A* Pathfinding Algorithm (https://en.wikipedia.org/wiki/A*_search_algorithm)
 /* A*: minimize this at each step: f(n) = g(n) + h(n)
@@ -90,9 +238,10 @@ class Maze {
    node values: {x, y, directions (possible neighbors): [], characteristics: [(S, E, *)]}
 */
 
+
 // Solve it
 // Load mazes from mazes.txt
-fs.readFile(MAZES, 'utf8', (err, mazes) => {
+fs.readFile(TEST_MAZE, 'utf8', (err, mazes) => {
     // probably need a variable for solutions to print to console
 
     if (err) console.error(err);
